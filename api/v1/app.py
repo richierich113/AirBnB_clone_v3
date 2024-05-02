11a716f3872935fd33360ff339e66e8a56c3a038
Richard Duah Boakye <richrichie784@gmail.com>
2024-05-02 13:36:38 +0000

    Update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -14,6 +14,16 @@ app.register_blueprint(app_views)
 # CORS(app, resources={r'/*': {'origins': '0.0.0.0'}})
 
 
+def not_found_errror_handler(e):
+    """a handler for 404 errors that returns a JSON-formatted
+    404 status code response
+    """
+    err_key = 'error'
+    err_val = "Not found"
+    err_code = 404
+    return jsonify({err_key: err_val}), err_code
+
+
 # Declare a method to handle app teardown
 @app.teardown_appcontext
 def teardown_appcontext(exception):
