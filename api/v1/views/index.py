ad7a4e02f41b7f9e3c89abd13af090ddb799a54f
Richard Duah Boakye <richrichie784@gmail.com>
2024-05-02 08:10:29 +0000

    Update index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,29 +3,7 @@
 """
 from api.v1.views import app_views
 from flask import jsonify
-from models import storage
 
-
-@app_views.route('/status', strict_slashes=False)
-def service_status():
-    """returns the status of the RESTful service"""
-    """ TODO check if this formatting is okay for json response with
-        holberton checker """
-    return jsonify({'status': 'OK'})
-
-
-@app_views.route('/stats', strict_slashes=False)
-def object_stats():
-    """returns the count of object types"""
-
-    classes = {
-        "amenities": "Amenity",
-        "cities": "City",
-        "places": "Place",
-        "reviews": "Review",
-        "states": "State",
-        "users": "User"
-        }
-    for k, v in classes.items():
-        classes[k] = storage.count(v)
-    return jsonify(classes)
+@app_views.route('/status', methods=['GET'])
+def get_status():
+    return jsonify({"status": "OK"})
