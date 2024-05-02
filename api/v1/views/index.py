d4a6e96991c2dafa6f91c3d65e5c408470121ef8
Richard Duah Boakye <richrichie784@gmail.com>
2024-05-02 08:51:39 +0000

    Update index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,7 +3,29 @@
 """
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
-@app_views.route('/status', methods=['GET'])
+
+@app_views.route('/status', strict_slashes=False)
 def get_status():
-    return jsonify({"status": "OK"})
+    """returns the status of the RESTful service"""
+    return jsonify({'status': 'OK'})
+
+
+@app_views.route('/stats', strict_slashes=False)
+def num_of_objs():
+    """endpoint that retrieves the number of each
+    objects by type
+    """
+
+    classes = {
+        "amenities": "Amenity",
+        "cities": "City",
+        "places": "Place",
+        "reviews": "Review",
+        "states": "State",
+        "users": "User"
+        }
+    for c_key, c_value in classes.items():
+        classes[c_key] = storage.count(c_value)
+    return jsonify(classes)
