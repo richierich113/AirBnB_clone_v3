76d95197dac611c10d0aeb9e67fa9ef91c642c7e
Richard Duah Boakye <richrichie784@gmail.com>
2024-05-02 08:21:48 +0000

    Update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,39 +1,33 @@
 #!/usr/bin/python3
-"""main app file for Flask instance in REST API
+"""main app file
 """
 from flask import Flask
-from flask import jsonify
-from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
 import os
 
+# Create a Flask application instance
 app = Flask(__name__)
+
+# Register the blueprint app_views to the Flask instance app
 app.register_blueprint(app_views)
-CORS(app, resources={r'/*': {'origins': '0.0.0.0'}})
 
 
-def page_not_found(e):
-    """404 error json response"""
-    return jsonify({'error': "Not found"}), 404
-
-
+# Declare a method to handle app teardown
 @app.teardown_appcontext
-def teardown_appcontext(exc=None):
-    """called on teardown of app contexts of flask
+def teardown_appcontext(exception):
+    """
+    Called on teardown of app contexts of Flask.
+    Closes the database connection.
     """
     storage.close()
 
 
+# Run the Flask server if the script is executed directly
 if __name__ == "__main__":
-    """run the app if the script is not being imported
-    """
-    app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
-    app.register_error_handler(404, page_not_found)
-    fetched_host = os.environ.get('HBNB_API_HOST')
-    fetched_port = os.environ.get('HBNB_API_PORT')
-    if fetched_host is None:
-        fetched_host = '0.0.0.0'
-    if fetched_port is None:
-        fetched_port = 5000
-    app.run(host=fetched_host, port=fetched_port, threaded=True)
+    # Retrieve host and port from environment variables
+    host = os.environ.get('HBNB_API_HOST', '0.0.0.0')
+    port = int(os.environ.get('HBNB_API_PORT', 5000))
+
+    # Run the Flask application with specified host, port, and threading option
+    app.run(host=host, port=port, threaded=True)
